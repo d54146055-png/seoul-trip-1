e3e86b9265c9070c32b526aa8bc6db10403051ba
d54146055-png <d54146055@gs.ncku.edu.tw>
2025-12-10 09:45:14 +0800

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // You can find this in the Firebase Console -> Project Settings
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyBPO5xK6-eFgItWQIEbYwsrUc3ZdAiq_y4",
+  authDomain: "seoultriphavefun.firebaseapp.com",
+  projectId: "seoultriphavefun",
+  storageBucket: "seoultriphavefun.firebasestorage.app",
+  messagingSenderId: "677388619970",
+  appId: "1:677388619970:web:40a53b51773c037881ab5b"
 };
 
 // Initialize Firebase
